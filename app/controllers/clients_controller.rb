4212e6f906d5147383911afdf7dd495e3e1bbba6
Spencer Johnson <sljohnson2222@gmail.com>
2022-04-27 14:49:31 -0500

    added front-end components: header navbar and mainContainer

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,6 @@
 class ClientsController < ApplicationController
+
+    def index 
+        render json: Client.all
+    end
 end
---
+++ client/src/components/MainContainer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function MainContainer() {
+    return(
+        <div className="main-container">MainContainer</div>
+    )
+}
+
+
+
+
+
+
+
+export default MainContainer
