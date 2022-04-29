bf9d002f1fffd43be78d9fcc3503bc21b087f9e5
Spencer Johnson <sljohnson2222@gmail.com>
2022-04-29 09:23:26 -0500

    got the detail client working with invoices

--- client/src/components/DetailClientContainer.js
+++ client/src/components/DetailClientContainer.js
@@ -5,38 +5,48 @@ import InvoiceSimpleCard from "./InvoiceSimpleCard";
 
 function DetailClientContainer() {
     const [showClient, setShowClient] = useState([])
-    console.log(showClient)
-    console.log(showClient.invoices)
+    // console.log(showClient)
+    // console.log(showClient.invoices)
+    
 
     // const [showClientProperty, setShowClientProperty] = useState([])
-    // const [showClientInvoices, setShowClientInvoices] = useState([])
+    const [showClientInvoices, setShowClientInvoices] = useState([])
     const { id } = useParams()
-    console.log(useParams())
+    // console.log(useParams())
 
     useEffect(() => {
         fetch(`/clients/${id}`)
             .then(res => res.json())
             .then(data => {
-                
+                // const mapShowClientInvoices = data.invoices.map(invoice => {
+                //     return (
+                //         <InvoiceSimpleCard key={invoice.id} invoice={invoice} />
+                //     )
+                // })
                 // console.log(data)
                 setShowClient(data)
-                
+                setShowClientInvoices(data.invoices)
+                console.log(data.invoices)
+            
                 
             })
     }, [id])
 
 
+    // let  mapShowClientInvoices = false
 
-    // const mapShowClientInvoices = showClient.invoices.map(invoice => {
-    //     return (
-    //         <InvoiceSimpleCard key={invoice.id} invoice={invoice} />
-    //     )
-    // })
+    // if (showClient.length) {
+        const mapShowClientInvoices = showClientInvoices.map(invoice => {
+            return (
+                <InvoiceSimpleCard key={invoice.id} invoice={invoice} />
+            )
+        })
+    // }  
 
     return (
         <div className="detail-client-container">
             <ClientInfoCard showClient={showClient}/>
-            {/* {mapShowClientInvoices} */}
+            {mapShowClientInvoices}
         </div>
     )
 }
